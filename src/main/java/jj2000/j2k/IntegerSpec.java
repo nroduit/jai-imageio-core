4fbaec81bd2d05862bd303955067dedf0d68151a
Nicolas Roduit <nicolas.roduit@jmicrovision.com>
2016-02-19 13:42:10 +0100

    Fix error on build doc

--- src/main/java/jj2000/j2k/IntegerSpec.java
+++ src/main/java/jj2000/j2k/IntegerSpec.java
@@ -93,7 +93,6 @@ public class IntegerSpec extends ModuleSpec{
      *
      * @param type The allowed specifications type
      *
-     * @param optName The name of the option to process
      *
      * */
     public IntegerSpec(int nt, int nc, byte type, J2KImageWriteParamJava wp, String values,
